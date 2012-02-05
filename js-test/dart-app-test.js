63df9954673aa03e2ea4f56668ee08b1c8db0dc7
unknown <klamping@klamping-PC.(none)>
2012-02-04 21:04:59 -0600

    Added test test file

--- js-test/dart-app-test.js
+++ js-test/dart-app-test.js
@@ -1,29 +1,59 @@
-TestCase('Function Assertion', {
-    "test game": function() {
-        assertFunction("Game should be a function", Game);
-    },
-    "test player": function() {
-        assertFunction("Player should be a function", Player);
-    },
-    "test round": function() {
-        assertFunction("Round should be a function", Round);
-    },
-    "test throw": function() {
-        assertFunction("Throw should be a function", Throw);
-    }
-});
+TestCase("Game Functionality", {
+    setUp:function () {
+        this.gameData = {
+            games : [
+                [{
+                    player: "Paul",
+                    throws: [1234567,1234567,2234567]
+                },
+                {
+                    player: "Kevin",
+                    throws: [1234567,1234567,1234567]
+                }],
+                [{
+                    player: "Paul",
+                    throws: [1234567,1234567,1234567]
+                },
+                {
+                    player: "Kevin",
+                    throws: [1234567,1234567,2234567]
+                }],
 
-TestCase('Game Propterty Assertion', {
-    "test players": function() {
-        assertObject("game should have two players", Game);
+            ],
+            hitValue: [15,16,17,18,19,20,25]
+        };
+        this.games = new Games(this.gameData);
     },
-    "test player": function() {
-        assertFunction("Player should be a function", Player);
+    "test that Game is an array": function () {
+        assertArray("Game", this.games);
     },
-    "test round": function() {
-        assertFunction("Round should be a function", Round);
+    "test that Games is two long": function () {
+        assertEquals("Number of games", "2", this.games.length);
     },
-    "test throw": function() {
-        assertFunction("Throw should be a function", Throw);
+    "test that all games have winner property" : function () {
+        // todo loop through the games
+        assertNotUndefined("Game winner", this.games[0].winner);
+    },
+    "test that Game 1 winner is Kevin" : function () {
+        assertEquals("Game winner", "Kevin", this.games[0].winner);
+    },
+    "test that Game 2 winner is Paul" : function () {
+        assertEquals("Game winner", "Paul", this.games[1].winner);
+    },
+    "test that all games have loser property" : function () {
+        // todo loop through the games
+        assertNotUndefined("Game loser", this.games[0].loser);
+    },
+    "test that Game loser is Paul" : function () {
+        assertEquals("Game loser", "Paul", this.game.loser);
+    },
+    "test that Game has players property":function () {
+        assertNotUndefined("Game players", this.game.players);
+    },
+    "test that Game players is an array":function () {
+        assertArray("Players", this.game.players);
+    },
+    "test that Game has two players":function () {
+        assertEquals("Number of players", "2", this.game.players.length);
     }
 });
